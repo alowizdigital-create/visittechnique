7f9422cb3c83201d3370d558e987da527a7c97c0
alowizdigital-create <alowizdigital@gmail.com>
2026-06-29 14:02:18 +0100

    Update view.php

--- templates/CashMovements/view.php
+++ templates/CashMovements/view.php
@@ -4,43 +4,13 @@
  * @var \App\Model\Entity\CashMovement $cashMovement
  */
 ?>
-<div class="row">
-    <div class="col-lg-3 col-md-4">
-        <div class="card bg-light mb-3">
-            <div class="card-header">
-                <h5 class="mb-0 text-dark"><?= __('Actions') ?></h5>
-            </div>
-            <div class="list-group list-group-flush">
-                <?= $this->Html->link(
-                    __('Modifier Mouvement'), 
-                    ['action' => 'edit', $cashMovement->id], 
-                    ['class' => 'list-group-item list-group-item-action list-group-item-info'] // Classe pour bouton d'édition
-                ) ?>
-                <?= $this->Form->postLink(
-                    __('Supprimer Mouvement'), 
-                    ['action' => 'delete', $cashMovement->id], 
-                    ['confirm' => __('Êtes-vous sûr de vouloir supprimer le mouvement # {0}?', $cashMovement->id), 'class' => 'list-group-item list-group-item-action list-group-item-danger'] // Classe pour bouton de suppression
-                ) ?>
-            </div>
-            <div class="list-group list-group-flush border-top">
-                <?= $this->Html->link(
-                    __('Liste des Mouvements'), 
-                    ['action' => 'index'], 
-                    ['class' => 'list-group-item list-group-item-action']
-                ) ?>
-                <?= $this->Html->link(
-                    __('Nouveau Mouvement'), 
-                    ['action' => 'add'], 
-                    ['class' => 'list-group-item list-group-item-action']
-                ) ?>
-            </div>
-        </div>
-    </div>
+
+<div class="row" style="margin-top:70px">
     
     <div class="col-lg-9 col-md-8">
         <div class="card shadow mb-4">
             <div class="card-header bg-primary text-white">
-                <h4 class="mb-0"><?= __('Détails du Mouvement de Caisse') ?></h4>
+                <h4 class="mb-0"><?= __('Détails') ?></h4>
             </div>
             <div class="card-body">
                 
@@ -58,10 +28,10 @@
                         </tr>
                         <tr>
                             <th scope="row"><?= __('Montant') ?></th>
-                            <td class="fw-bold"><?= $this->Number->currency($cashMovement->montant) ?></td>
+                            <td class="fw-bod"><?= __($cashMovement->montant) ?> Fcfa</td>
                         </tr>
                         <tr>
-                            <th scope="row"><?= __('Créé le') ?></th>
+                            <th scope="row"><?= __('Date') ?></th>
                             <td><?= h($cashMovement->created->nice()) ?></td>
                         </tr>
                         </tbody>
