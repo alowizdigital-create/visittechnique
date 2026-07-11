d96ecd1f0b4e12c703351d2b9c884fe5c66bd11d
alowizdigital-create <alowizdigital@gmail.com>
2026-07-11 15:57:34 +0200

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org) 
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
