96f9a471508896e2fd8b83eed0576cff24e44ad0
alowizdigital-create <alowizdigital@gmail.com>
2026-07-13 01:32:14 +0100

    Update CashBoxesController.php

--- src/Controller/CashBoxesController.php
+++ src/Controller/CashBoxesController.php
@@ -216,8 +216,7 @@ class CashBoxesController extends AppController
         throw new \Cake\Http\Exception\NotFoundException('Utilisateur introuvable');
     }
 
-    // $startupId = $userData->startup_id ?? null;
-    $startupId = 12;
+    $startupId = $userData->startup_id ?? null;
 
     // 🔹 Récupération des caisses
     if (in_array($userData->role, ['admin', 'directeur'])) {
